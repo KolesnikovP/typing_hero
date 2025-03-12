ba2e3da0306ace4b35347ab64bb7401a53f7588f
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-11 22:13:26 -0500

    STYLES: setup styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css'
+// import './App.scss'
 import { Header } from '@widgets/Header'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
      <Header/> 
       <h1>Vite + React</h1>
       <div className="card">
       </div>
-    </>
+    </div>
   )
 }
 
