cd55d5fa0ac1a1c60709905560b14b9f54bbccb0
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-11 22:37:40 -0500

    checking styles

--- client/src/widgets/Header/ui/Header.tsx
+++ client/src/widgets/Header/ui/Header.tsx
@@ -2,7 +2,14 @@ import cls from './Header.module.scss'
 export const Header = () => {
   return (
     <header className={cls.Header}>
-     header of navbar should be here 
+      <div className={cls.HeaderItem}>
+        <div className={cls.MenuItem}>logo</div>
+        <div className={cls.MenuItem}>settings</div>
+      </div>
+      <div className={cls.HeaderItem}>
+        <div className={cls.MenuItem}>notifications</div>
+        <div className={cls.MenuItem}>Login</div>
+      </div>
     </header>
   )
 }
