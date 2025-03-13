a0cbeed152e71f70897076a042ae91f5608e8a72
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-13 00:15:25 -0500

    cleaning

--- client/src/features/TypingWindow/ui/TypingWindow.tsx
+++ client/src/features/TypingWindow/ui/TypingWindow.tsx
@@ -5,6 +5,7 @@ import cls from './TypingWindow.module.scss'
 
 const mockedText = getMockedTypingText()
 const ignoreKeysList: Set<string> = new Set(['Shift', 'Control', 'Meta', 'Alt', 'Tab'])
+
 export function TypingWindow() {
   const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
   const [mistakenIndexes, setMistakenIndexes] = useState(new Set<number>());
@@ -12,15 +13,13 @@ export function TypingWindow() {
   function onKeyDownHandler(e: KeyboardEvent<HTMLDivElement>){
     if(!ignoreKeysList.has(e.key)) {
       if(e.key !== mockedText[currentLetterIndex]) {
-        console.log('event: ' + e.key, 'target: ' + mockedText[currentLetterIndex])
         setMistakenIndexes(prev => new Set(prev).add(currentLetterIndex))
       } else {
         setCurrentLetterIndex(prev => prev + 1)
       }
     } 
-
-    console.log('key pressed down>>>> ', e.key)
   }
+
   return (
     <div className={cls.Container} tabIndex={0} onKeyDown={onKeyDownHandler} >
       {mockedText.map((el, index) => (
