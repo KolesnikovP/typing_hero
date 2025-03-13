1bec08dc207911c038a7d989a0bb273296ec7c19
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-13 16:37:54 -0400

    not perfect scrolling

--- client/src/features/TypingWindow/ui/TypingWindow.tsx
+++ client/src/features/TypingWindow/ui/TypingWindow.tsx
@@ -1,19 +1,28 @@
-import { KeyboardEvent, useState } from 'react'
+import { KeyboardEvent, useRef, useState } from 'react'
 import { getMockedTypingText } from '../mockText'
 import { CustomSpan } from './CustomSpan/CustomSpan'
 import cls from './TypingWindow.module.scss'
 
-const mockedText = [...getMockedTypingText(), ...getMockedTypingText(), ...getMockedTypingText(), ...getMockedTypingText()]
+const mockedText = [...getMockedTypingText(), ...getMockedTypingText()]
 const ignoreKeysList: Set<string> = new Set(['Shift', 'Control', 'Meta', 'Alt', 'Tab'])
 
 export function TypingWindow() {
   const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
   const [mistakenIndexes, setMistakenIndexes] = useState(new Set<number>());
+  const spanRefs = useRef<(HTMLSpanElement | null)[]>([])
+  const containerRef = useRef<HTMLDivElement | null>(null) // Ref for the scrolling containerRef
 
   function onKeyDownHandler(e: KeyboardEvent<HTMLDivElement>){
     if(!ignoreKeysList.has(e.key)) {
       if(e.key !== mockedText[currentLetterIndex]) {
         setMistakenIndexes(prev => new Set(prev).add(currentLetterIndex))
+        // Scroll to the currently typed letter
+        spanRefs.current[currentLetterIndex]?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+          inline: "start",
+        })
+
       } else {
         setCurrentLetterIndex(prev => prev + 1)
       }
@@ -21,10 +30,16 @@ export function TypingWindow() {
   }
 
   return (
-    <div className={cls.Container} tabIndex={0} onKeyDown={onKeyDownHandler} >
+    <div
+      ref={containerRef}
+      className={cls.Container}
+      tabIndex={0}
+      onKeyDown={onKeyDownHandler}
+    >
       {mockedText.map((el, index) => (
           <CustomSpan
             key={index}
+            ref={(el) => (spanRefs.current[index] = el)} // Assign ref
             isPointerOn={index === currentLetterIndex }
             isTyped={currentLetterIndex <= index}
             isMistakenKey={mistakenIndexes.has(index)} // Check if this index was mistaken
