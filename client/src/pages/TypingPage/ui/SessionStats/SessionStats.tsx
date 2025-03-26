d88f752bdf7a173c3eb5e2c2cb502cb37a9075be
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-26 16:52:49 -0500

    CPM

--- client/src/pages/TypingPage/ui/SessionStats/SessionStats.tsx
+++ client/src/pages/TypingPage/ui/SessionStats/SessionStats.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 type SessionStatsProps = {
   lettersTyped: number;
   mistakesCount: number;
+  givenTime: number;
 }
 export const SessionStats = (props : SessionStatsProps) => {
 
-  const {lettersTyped, mistakesCount} = props; 
+  const {lettersTyped, givenTime, mistakesCount} = props; 
+  const getCPM = useCallback(() => {
+    const timeInMinutes = givenTime / 60
+    const result = lettersTyped / timeInMinutes;
+    return result;
+  }, [lettersTyped, givenTime, mistakesCount])
   return (
     <div>
-      letters typed: {lettersTyped} | made mistakes: {mistakesCount}     
+      letters typed: {lettersTyped} | made mistakes: {mistakesCount} | session time: {givenTime}s     
+     <p>your CPM: {getCPM()}</p> 
     </div>
   )
 }
