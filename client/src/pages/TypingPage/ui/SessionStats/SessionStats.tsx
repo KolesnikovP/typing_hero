1717dd01fb9382ee6b43caa7baee3d69d109752c
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-26 17:06:58 -0500

    wpm

--- client/src/pages/TypingPage/ui/SessionStats/SessionStats.tsx
+++ client/src/pages/TypingPage/ui/SessionStats/SessionStats.tsx
@@ -12,10 +12,14 @@ export const SessionStats = (props : SessionStatsProps) => {
     const result = lettersTyped / timeInMinutes;
     return result;
   }, [lettersTyped, givenTime, mistakesCount])
+
+  const cpm = getCPM();
+  const wpm = Math.floor(cpm / 5);
   return (
     <div>
       letters typed: {lettersTyped} | made mistakes: {mistakesCount} | session time: {givenTime}s     
-     <p>your CPM: {getCPM()}</p> 
+     <p>CPM: {cpm}</p> 
+     <p>WPM: {wpm}</p> 
     </div>
   )
 }
