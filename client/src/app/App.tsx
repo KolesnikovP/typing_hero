aefbcfb5f9f4c80f5bd9fbb97480e78ead5138f5
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-03-29 08:27:02 -0500

    error boundaries

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,15 +1,27 @@
 // import './App.scss'
 import { TypingPage } from '@/pages/TypingPage'
 import { Header } from '@widgets/Header'
+import { Suspense } from 'react'
 
 function App() {
 
   return (
     <div className='app'>
       <Header/> 
-      <div className="" style={{display: 'flex', flexDirection: 'column', alignItems: 'center' ,justifyContent: 'center'}}>
+      <Suspense>
+        <div 
+          className="content-page" 
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
         <TypingPage/>
       </div>
+
+      </Suspense>
+
     </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/app/styles/index.scss';
 import App from '@/app/App.tsx'
+import { BrowserRouter } from 'react-router';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>    
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
   </StrictMode>,
 )
