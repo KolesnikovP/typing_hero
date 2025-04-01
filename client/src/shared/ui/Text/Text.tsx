a980fa85ded3ff4bb9f520bfeaa439ea7c5b7ef6
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-04-01 01:46:16 -0500

    shared text

---
+++ client/src/shared/ui/Text/Text.tsx
@@ -0,0 +1,69 @@
+import { memo } from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import cls from './Text.module.scss';
+
+export type TextTheme = 'primary' | 'inverted' | 'error'
+
+export type TextAlign = 'right' | 'left'| 'center'
+
+export type TextSize = 'size_s' | 'size_m' | 'size_l'
+
+interface TextProps {
+  className?: string;
+  title?: string;
+  text?: string;
+  theme?: TextTheme;
+  align?: TextAlign;
+  size?: TextSize;
+
+  'data-testid'?: string;
+}
+
+type HeaderTagType = 'h1' | 'h2' | 'h3';
+
+const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
+  size_s: 'h3',
+  size_m: 'h2',
+  size_l: 'h1',
+};
+
+export const Text = memo((props: TextProps) => {
+  const {
+    className,
+    text,
+    title,
+    theme = 'primary',
+    align = 'left',
+    size = 'size_m',
+    'data-testid': dataTestId = 'Text',
+  } = props;
+
+  const HeaderTag = mapSizeToHeaderTag[size];
+
+  const mods: Mods = {
+    [cls[theme]]: true,
+    [cls[align]]: true,
+    [cls[size]]: true,
+  };
+
+  return (
+    <div className={classNames(cls.Text, mods, [className])}>
+      {title && (
+        <HeaderTag
+          className={cls.title}
+          data-testid={`${dataTestId}.Header`}
+        >
+          {title}
+        </HeaderTag>
+      )}
+      {text && (
+        <p
+          className={cls.text}
+          data-testid={`${dataTestId}.Paragraph`}
+        >
+          {text}
+        </p>
+      )}
+    </div>
+  );
+});
