d5ce19a368c832ea6ce21275fc43920e90226122
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-04-01 01:42:49 -0500

    reusable modal

---
+++ client/src/shared/ui/Modal/ui/Modal.tsx
@@ -0,0 +1,58 @@
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import styles from './Modal.module.scss';
+import { JSX } from 'react';
+
+type ModalProps = HTMLDivElement & {
+  title?: string;
+  description?: string;
+  children: JSX.Element;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function Modal(props: ModalProps) {
+  const {title, description, isOpen, onClose, children} = props;
+
+  return (
+    <>
+      <AnimatePresence>
+        {isOpen && (
+          <Dialog static open={isOpen} onClose={onClose} className="relative z-50">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className={styles.overlay}
+            />
+            <div className={styles.container}>
+              <DialogPanel
+                as={motion.div}
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.95 }}
+                className={styles.panel}
+              >
+                {
+                title &&
+                <DialogTitle className={styles.title}>
+                  {title}
+                </DialogTitle>
+                }
+
+                {description &&
+                <Description className={styles.description}>
+                  {description} 
+                </Description>
+                }
+                {children}
+              </DialogPanel>
+            </div>
+          </Dialog>
+        )}
+      </AnimatePresence>
+    </>
+  );
+}
+
+export default Modal;
